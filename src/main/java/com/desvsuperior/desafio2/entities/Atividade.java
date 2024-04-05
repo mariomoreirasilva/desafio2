0f9389c674da51b3520e5d211970142d1545df81
mariomoreirasilva <mario@desban.org.br>
2024-04-05 16:02:46 -0300

    Atividade_Categoria

---
+++ src/main/java/com/desvsuperior/desafio2/entities/Atividade.java
@@ -0,0 +1,75 @@
+package com.desvsuperior.desafio2.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_atividade")
+public class Atividade {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private String descriçao;
+	private Double preco;
+	@ManyToOne
+	@JoinColumn(name = "id_categoria")
+	private Categoria categoria;
+	
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public Atividade() {		
+	}
+
+	public Atividade(Integer id, String nome, String descriçao, Double preco) {		
+		this.id = id;
+		this.nome = nome;
+		this.descriçao = descriçao;
+		this.preco = preco;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescriçao() {
+		return descriçao;
+	}
+
+	public void setDescriçao(String descriçao) {
+		this.descriçao = descriçao;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	
+
+}
