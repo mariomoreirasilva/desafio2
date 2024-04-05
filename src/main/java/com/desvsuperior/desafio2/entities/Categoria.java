28f7e19b57c88c0c89f5e1cb42f068a42f77cb51
mariomoreirasilva <mario@desban.org.br>
2024-04-05 15:36:30 -0300

    Banco H2 configurado

---
+++ src/main/java/com/desvsuperior/desafio2/entities/Categoria.java
@@ -0,0 +1,32 @@
+package com.desvsuperior.desafio2.entities;
+
+public class Categoria {
+	private Integer id;
+	private String descricao;
+	
+	public Categoria() {		
+	}
+
+	public Categoria(Integer id, String descricao) {		
+		this.id = id;
+		this.descricao = descricao;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+
+}
